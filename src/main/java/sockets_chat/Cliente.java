2e9a4e165bfd39fcea84a69cb07b6b48aa9d12d0
Juan-Estevez <juank2001estevez@gmail.com>
2021-09-27 17:33:35 -0500

    Updating Chat Cliente 27/09/2021

--- src/main/java/sockets_chat/Cliente.java
+++ src/main/java/sockets_chat/Cliente.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -32,7 +33,7 @@ public class Cliente {
 class MarcoCliente extends JFrame {
 
     public MarcoCliente() {
-        this.setTitle("Chat básico CLIENTE");
+        this.setTitle("Chat básico");
         this.setBounds(600, 300, 280, 350);
         PanelMarcoCliente panel = new PanelMarcoCliente();
         this.add(panel);
@@ -45,21 +46,30 @@ class MarcoCliente extends JFrame {
 class PanelMarcoCliente extends JPanel {
 
     // Variables
-    private final JTextField campo1;
+    private final JTextField campo1, nick, ip;
     private final JButton boton;
+    private final JTextArea area_texto;
 
     // Constructor de clase
     public PanelMarcoCliente() {
-        JLabel texto = new JLabel("CLIENTE");
-        this.add(texto);
+        nick = new JTextField(5);
+        ip = new JTextField(8);
+        JLabel texto = new JLabel("--- CHAT ---");
+        area_texto = new JTextArea(12, 20);
         campo1 = new JTextField(20);
-        this.add(campo1);
         boton = new JButton("ENVIAR");
 
         // Instancia de la clase interna que mediante un Socket envía texto
         EnviarTexto evento = new EnviarTexto();
         // Añadiendo el evento (clase interna) al botón, para que al presionar dicho botón, se inicie el Socket
         boton.addActionListener(evento);
+
+        // Añadiendo los componenetes Swing al Panel
+        this.add(nick);
+        this.add(texto);
+        this.add(ip);
+        this.add(area_texto);
+        this.add(campo1);
         this.add(boton);
     }
 
@@ -72,14 +82,49 @@ class PanelMarcoCliente extends JPanel {
                 // Creación del Socket
                 Socket socket = new Socket("192.168.56.1", 9999);
 
-                // Creación del flujo de datos que viajará por el Socket
-                DataOutputStream flujo_datos_salida = new DataOutputStream(socket.getOutputStream());
-                flujo_datos_salida.writeUTF(campo1.getText()); // Escribiendo el mensaje
-                flujo_datos_salida.close(); // Cerrando el flujo de datos
+                // Instancia de la clase PaqueteEnvio
+                PaqueteEnvio datos = new PaqueteEnvio();
+
+                // Seteando atributos de la clase Paquete Envio
+                datos.setNick(nick.getText());
+                datos.setIp(ip.getText());
+                datos.setMensaje(campo1.getText());
             } catch (IOException ex) {
                 System.out.println(ex.getMessage());
             }
         }
 
     }
+}
+
+// Clase encargada de encapsular los atributos a enviar como mensaje (Nick, IP, Mensaje)
+class PaqueteEnvio {
+
+    private String nick, ip, mensaje;
+
+    // Getters and Setters
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
 }
